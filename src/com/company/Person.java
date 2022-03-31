429f5bb8635118360e3a0214a24cf76df223bdf1
Daniel PerisPortal <danielperis1998@gmail.com>
2022-03-31 12:58:07 +0200

    Creació del projecte

---
+++ src/com/company/Person.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Person {
+    public String name;
+    public String surname;
+    public int age;
+    public boolean vegan;
+
+    public Person(String name, String surname, int age, boolean vegan) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.vegan = vegan;
+    }
+
+
+}
