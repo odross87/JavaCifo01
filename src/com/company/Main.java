b2c16bade37dee3ca8be4a3f0b6141faa08a0a08
Daniel PerisPortal <danielperis1998@gmail.com>
2022-03-31 13:13:22 +0200

    Creació hola david

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person david = new Person("David", "Garcia", 27, false);
+        System.out.println(david.name + " " + david.surname + " te " + david.age + " anys.");
+    }
+}
--- src/com/company/Test.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Test {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
